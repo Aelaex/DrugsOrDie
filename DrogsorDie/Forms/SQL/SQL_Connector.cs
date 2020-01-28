c1aaf2478f43651f49c3d0701a39e5acd267467f
Fabian P <mysterytyp@gmail.com>
2020-01-28 11:58:40 +0100

    Version Fixing

--- DrogsorDie/Forms/SQL/SQL_Connector.cs
+++ DrogsorDie/Forms/SQL/SQL_Connector.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 namespace DrogsorDie.Forms.SQL
 {
     class SQL_Connector
     {
-        
+        public static MySqlConnection getDefaultLocalConnection()
+        {
+            String connString = "Server=" + "localhost" + ";Database=" + "default_schema"
+                + ";port=" + "3306" + ";User Id=" + "user" + ";password=" + "user";
+
+            return new MySqlConnection(connString);
+        }
+        public static MySqlConnection
+                 GetDBConnection(string host, int port, string database, string username, string password)
+        {
+            // Connection String.
+            String connString = "Server=" + host + ";Database=" + database
+                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            return conn;
+        }
     }
 }
