8f8d72fdcdd674010ec1363b4389eeac2f35e846
Alaxandar <33285873+Wezelal@users.noreply.github.com>
2020-01-28 08:16:57 +0100

    Program.cs Gänder um Forms zu starten

--- DrogsorDie/Program.cs
+++ DrogsorDie/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DrogsorDie.Forms.GUI;
 
 namespace DrogsorDie
 {
@@ -16,7 +17,7 @@ namespace DrogsorDie
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Patient());
         }
     }
 }
