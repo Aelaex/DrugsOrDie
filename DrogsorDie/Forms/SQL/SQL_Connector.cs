01d634ab452e1f778d89eefe9d428603fc0fe3ac
Fabian.Pfaff <mysterytyp@gmail.com>
2020-02-06 12:14:14 +0100

    mySQL InitialisierungsSkript

--- DrogsorDie/Forms/SQL/SQL_Connector.cs
+++ DrogsorDie/Forms/SQL/SQL_Connector.cs
@@ -33,7 +33,7 @@ namespace DrogsorDie.Forms.SQL
         }
         public static void initializeDefaultLocalConnection()
         {
-            initializeDBConnection("localhost", 3306, "default_schema", "user", "user");
+            initializeDBConnection("localhost", 3306, "default_schema", "DBUser", "user");
         }
         public static void initializeDBConnection(string host, int port, string database, string username, string password)
         {
