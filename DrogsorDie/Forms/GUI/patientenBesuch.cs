55e827aa3a269615642b745ecd97da42ce0d17c5
Alaxandar <33285873+Wezelal@users.noreply.github.com>
2020-02-06 14:14:17 +0100

    APtienbesuch

---
+++ DrogsorDie/Forms/GUI/patientenBesuch.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace DrogsorDie.Forms.GUI
+{
+    public partial class patientenBesuch : Form
+    {
+        public patientenBesuch()
+        {
+            InitializeComponent();
+        }
+    }
+}
