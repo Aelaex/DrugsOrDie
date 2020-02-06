d24e2d2e8ae815b9f324f48d1d7b13b2ab6842ac
Alaxandar <33285873+Wezelal@users.noreply.github.com>
2020-02-06 12:25:47 +0100

    comit

--- DrogsorDie/Forms/GUI/Patient.cs
+++ DrogsorDie/Forms/GUI/Patient.cs
@@ -16,15 +16,30 @@ namespace DrogsorDie.Forms.GUI
         public Patient()
         {
             InitializeComponent();
-            PatientL patien = new PatientL("", "");
+            PatientL patient = new PatientL("", "");
+            textBoxVorname.Text = patient.Vorname;
+            textBoxNachname.Text = patient.Nachname;
+            textBoxPLZ.Text = Convert.ToString( patient.Plz);
+            textBoxStraße.Text = patient.Straße;
+            textBoxTelefon.Text = patient.Telefon;
+            textBoxGerburstag.Text = patient.Geburtstag.ToString("dd/MM/yyyy");
+            textBoxGeschlecht.Text = patient.Geschlecht;
             
-            
+
         }
 
         private void buttonPatientenliste_Click(object sender, EventArgs e)
         {
             Patientenliste pliste = new Patientenliste();
             pliste.Show();
+            
+
+        }
+
+        private void buttonAllergien_Click(object sender, EventArgs e)
+        {
+            DateTime test = new DateTime(2010,12,12);
+            textBoxWohnort.Text = test.ToString("dd/MM/yyyy");
         }
     }
 }
