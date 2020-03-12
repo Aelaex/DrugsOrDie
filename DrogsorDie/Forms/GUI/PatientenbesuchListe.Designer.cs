6c5b9ce13a03be38bd6a02573182be66f0d812ec
Alaxandar <33285873+Wezelal@users.noreply.github.com>
2020-03-12 11:26:21 +0100

    Update PatientenbesuchListe.Designer.cs

--- DrogsorDie/Forms/GUI/PatientenbesuchListe.Designer.cs
+++ DrogsorDie/Forms/GUI/PatientenbesuchListe.Designer.cs
@@ -34,22 +34,23 @@ namespace DrogsorDie.Forms.GUI
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Datrum = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode =
-                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {this.Id, this.Datrum});
-            this.dataGridView1.Location = new System.Drawing.Point(35, 28);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Datrum});
+            this.dataGridView1.Location = new System.Drawing.Point(30, 24);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(355, 434);
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(304, 376);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellDoubleClick +=
-                new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // Id
             // 
@@ -63,15 +64,16 @@ namespace DrogsorDie.Forms.GUI
             // 
             // PatientenbesuchListe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(521, 504);
+            this.ClientSize = new System.Drawing.Size(447, 437);
             this.Controls.Add(this.dataGridView1);
             this.Name = "PatientenbesuchListe";
             this.Text = "PatientenbesuchListe";
-            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+
         }
 
         #endregion
